f47c718f35497b3fd1d63768bdcc002d024a0112
Jerome <je.gelmar@gmx.de>
2022-08-27 02:19:06 +0200

    VTW-1: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="w-screen h-screen flex flex-col">
+        <nav class="bg-red-500 p-3 text-center text-white text-2xl font-bold"><router-link to="/">Home</router-link></nav>
+        <router-view />
+    </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-full h-full bg-green-500 flex justify-center items-center text-4xl font-bold">This is the home view</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
